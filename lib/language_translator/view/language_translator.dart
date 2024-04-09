4e65537296a64f9c8d992e1d63eb939bb91de72f
Narayan Nalbhe <145420297+narayannalbhe1@users.noreply.github.com>
2024-04-09 16:04:42 +0530

    9 april master commit lagnuage translator

---
+++ lib/language_translator/view/language_translator.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
+
+class LanguageTranslator extends StatefulWidget {
+  const LanguageTranslator({super.key});
+
+  @override
+  State<LanguageTranslator> createState() => _LanguageTranslatorState();
+}
+
+class _LanguageTranslatorState extends State<LanguageTranslator> {
+  var languages = ['Hindi', 'English', 'Marathi'];
+  var originLanguage = 'From';
+  var destinationLanguage = 'To';
+  var output = '';
+  TextEditingController languageController = TextEditingController();
+
+  void translate(String src, String dest, String input) async {
+    if (src == '--' || dest == '--') {
+      setState(() {
+        output = 'Failed to translate: Select languages';
+      });
+      return;
+    }
+
+    GoogleTranslator translator = GoogleTranslator();
+    var translation = await translator.translate(input, from: src, to: dest);
+
+    setState(() {
+      output = translation.text;
+    });
+  }
+
+  String getLanguageCode(String language) {
+    switch (language) {
+      case "English":
+        return 'en';
+      case 'Hindi':
+        return 'hi';
+      case "Marathi":
+        return 'mar';
+      default:
+        return "--";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text(
+          'Language Translator',
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DropdownButton(
+                      focusColor: Colors.white,
+                      iconDisabledColor: Colors.white,
+                      iconEnabledColor: Colors.white,
+                      hint: Text(
+                        originLanguage,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      dropdownColor: Colors.white,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      items: languages.map((String dropDownStringItem) {
+                        return DropdownMenuItem(
+                          child: Text(dropDownStringItem),
+                          value: dropDownStringItem,
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        setState(() {
+                          originLanguage = value!;
+                        });
+                      }),
+
+                  DropdownButton(
+                      focusColor: Colors.white,
+                      iconDisabledColor: Colors.white,
+                      iconEnabledColor: Colors.white,
+                      hint: Text(
+                        destinationLanguage,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      dropdownColor: Colors.white,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      items: languages.map((String dropDownStringItem) {
+                        return DropdownMenuItem(
+                          child: Text(dropDownStringItem),
+                          value: dropDownStringItem,
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        setState(() {
+                          destinationLanguage = value!;
+                        });
+                      }),
+                ],
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: languageController,
+                cursorColor: Colors.white,
+                autofocus: false,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'Please enter your text...',
+                  labelStyle: TextStyle(fontSize: 15, color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1),
+                  ),
+                  errorStyle: TextStyle(color: Colors.red, fontSize: 15),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter value';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.white),
+                onPressed: () {
+                  translate(
+                    getLanguageCode(originLanguage),
+                    getLanguageCode(destinationLanguage),
+                    languageController.text.toString(),
+                  );
+                },
+                child: Text('Translate'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                '\n$output',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/qr_code_scanner_project/view/home_page.dart
+++ lib/qr_code_scanner_project/view/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projects/language_translator/view/language_translator.dart';
 import 'package:flutter_projects/qr_code_scanner_project/view/generate_qr_code.dart';
 import 'package:flutter_projects/qr_code_scanner_project/view/scan_qr_code.dart';
 
@@ -14,13 +15,22 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QR code Scanner and Generator'),
+        title: Text('Flutter Projects',style: TextStyle(
+          color: Colors.white
+        ),),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            SizedBox(height: 10),
+            Text('QR code Scanner and Generator :',style: TextStyle(
+              color: Colors.blueAccent,fontWeight: FontWeight.bold,
+               fontSize: 16
+            ),),
             ElevatedButton(
                 onPressed: (){
                   setState(() {
@@ -44,6 +54,23 @@ class _HomePageState extends State<HomePage> {
                 child: Text(
                     'Generate QR Code '
                 )),
+            SizedBox(height: 20),
+            Text('Language Translator :',style: TextStyle(
+                color: Colors.deepPurple,fontWeight: FontWeight.bold,
+                fontSize: 16
+            ),),
+            ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    Navigator.push(context, MaterialPageRoute(builder: (context){
+                      return LanguageTranslator();
+                    })) ;
+                  });
+                },
+                child: Text(
+                    'Language Translator'
+                )),
+
           ],
         ),
       ),
